5ea2cde7bbf6389c1a5735b206cf55a02d54edc3
Scot Dalton <scotdalton@gmail.com>
2014-08-22 16:08:45 -0400

    Finish fleshing out the LinkedList

--- lib/dayglo/linked_list.rb
+++ lib/dayglo/linked_list.rb
@@ -1,4 +1,90 @@
+require_relative 'linked_list/item'
 module Dayglo
+  # Pure Ruby implementation of a LinkedList
   class LinkedList
+
+    # Pushes an object onto the head of the linked list
+    # Insert on a list of n elements is O(1)
+    def insert(object)
+      old_head = head
+      @head = LinkedList::Item.new(object)
+      @tail ||= head
+      head.next = old_head unless old_head.nil?
+      nil
+    end
+
+    # Finds and returns an object from the linked list
+    # Search on a list of n elements is O(n)
+    def search(object)
+      item = find_item_by_object(head, object)
+      item.object unless item.nil?
+    end
+
+    # Deletes and returns an object in the linked list
+    # Delete on a list of n elements is O(n)
+    def delete(object)
+      deleted_item = find_item_by_object(head, object)
+      unless deleted_item.nil?
+        if deleted_item.tail?
+          @head = nil
+        else
+          previous_item = find_item_by_next_item(head, deleted_item)
+          # Link the deleted item's next item to the previous item
+          previous_item.next = deleted_item.next
+        end
+        deleted_item.object
+      end
+    end
+
+    # Returns the last element inserted into the list
+    # Minimum on a list of n elements is O(1)
+    def minimum
+      head.object unless head.nil?
+    end
+
+    # Returns the first element inserted into the list
+    # Maximum on a list of n elements is O(1)
+    def maximum
+      tail.object unless tail.nil?
+    end
+
+    # Boolean indicating that the list is empty
+    def empty?
+      head.nil?
+    end
+
+    # String representation of the list
+    def to_s
+      "#{items_to_s(head)}"
+    end
+
+    private
+    attr_reader :head, :tail
+
+    def items_to_s(item)
+      unless item.nil?
+        (item.tail?) ? "#{item}" : "#{item}, #{items_to_s(item.next)}"
+      end
+    end
+
+    def find_item_by_next_item(item, next_item)
+      unless item.nil?
+        if item.next == next_item
+          item
+        else
+          find_item_by_next_item(item.next, next_item)
+        end
+      end
+    end
+
+    def find_item_by_object(item, object)
+      unless item.nil?
+        if item.object == object
+          item
+        else
+          find_item_by_object(item.next, object)
+        end
+      end
+    end
   end
 end
--- spec/dayglo/linked_list_spec.rb
+++ spec/dayglo/linked_list_spec.rb
@@ -1,7 +1,156 @@
 require 'spec_helper'
 module Dayglo
   describe LinkedList do
+    let(:object) { Object.new }
     subject(:linked_list) { LinkedList.new }
     it { should be_a LinkedList }
+    describe '#empty?' do
+      subject { linked_list.empty? }
+      context 'when there are no items in the list' do
+        it { should be true }
+      end
+      context 'when there are items in the list' do
+        before { linked_list.insert(object) }
+        it { should be false }
+      end
+    end
+    describe '#insert' do
+      subject { linked_list.insert(object) }
+      it { should be nil }
+      it 'should insert an item into the list' do
+        expect(linked_list).to be_empty
+        subject
+        expect(linked_list).not_to be_empty
+      end
+    end
+    describe '#search' do
+      subject { linked_list.search(object) }
+      context 'when there is one item in the list' do
+        context 'and the item matches the given object' do
+          before { linked_list.insert(object) }
+          it { should eq object }
+        end
+        context 'but the item does not match the given object' do
+          before { linked_list.insert(Object.new) }
+          it { should be nil }
+        end
+      end
+      context 'when there are many items in the list' do
+        context 'and only one item matches the given object' do
+          before { 5.times { linked_list.insert(Object.new) } }
+          before { linked_list.insert(object) }
+          before { 5.times { linked_list.insert(Object.new) } }
+          it { should eq object }
+        end
+        context 'and several items match the given object' do
+          before { 5.times { linked_list.insert(Object.new) } }
+          before { 3.times { linked_list.insert(object) } }
+          before { 5.times { linked_list.insert(Object.new) } }
+          it { should eq object }
+        end
+        context 'but no items match the given object' do
+          before { 5.times { linked_list.insert(Object.new) } }
+          it { should be nil }
+        end
+      end
+    end
+    describe '#delete' do
+      subject { linked_list.delete(object) }
+      context 'when there is one item in the list' do
+        context 'and the item matches the given object' do
+          before { linked_list.insert(object) }
+          it { should eq object }
+          it 'should not find the object when searching' do
+            expect(linked_list.search(object)).to eq object
+            subject
+            expect(linked_list.search(object)).to be nil
+          end
+        end
+        context 'but the item does not match the given object' do
+          before { linked_list.insert(Object.new) }
+          it { should be nil }
+          it 'should not find the object when searching' do
+            expect(linked_list.search(object)).to be nil
+            subject
+            expect(linked_list.search(object)).to be nil
+          end
+        end
+      end
+      context 'when there are many items in the list' do
+        context 'and only one item matches the given object' do
+          before { 5.times { linked_list.insert(Object.new) } }
+          before { linked_list.insert(object) }
+          before { 5.times { linked_list.insert(Object.new) } }
+          it { should eq object }
+          it 'should not find the object when searching' do
+            expect(linked_list.search(object)).to eq object
+            subject
+            expect(linked_list.search(object)).to be nil
+          end
+        end
+        context 'and several items match the given object' do
+          before { 5.times { linked_list.insert(Object.new) } }
+          before { 3.times { linked_list.insert(object) } }
+          before { 5.times { linked_list.insert(Object.new) } }
+          it { should eq object }
+          it 'should find the object when searching' do
+            expect(linked_list.search(object)).to eq object
+            subject
+            expect(linked_list.search(object)).to eq object
+          end
+        end
+        context 'but no items match the given object' do
+          before { 5.times { linked_list.insert(Object.new) } }
+          it { should be nil }
+          it 'should not find the object when searching' do
+            expect(linked_list.search(object)).to be nil
+            subject
+            expect(linked_list.search(object)).to be nil
+          end
+        end
+      end
+    end
+    describe '#minimum' do
+      subject { linked_list.minimum }
+      context 'when there are no items' do
+        it { should eq nil}
+      end
+      context 'when there is one item' do
+        before { linked_list.insert('item1') }
+        it { should eq 'item1'}
+      end
+      context 'when there is more than one item' do
+        before { 3.times { |n| linked_list.insert("item#{n+1}") } }
+        it { should eq 'item3' }
+      end
+    end
+    describe '#maximum' do
+      subject { linked_list.maximum }
+      context 'when there are no items' do
+        it { should eq nil}
+      end
+      context 'when there is one item' do
+        before { linked_list.insert('item1') }
+        it { should eq 'item1'}
+      end
+      context 'when there is more than one item' do
+        before { 3.times { |n| linked_list.insert("item#{n+1}") } }
+        it { should eq 'item1' }
+      end
+    end
+    describe '#to_s' do
+      subject { linked_list.to_s }
+      context 'when there are no items' do
+        it { should eq ''}
+      end
+      context 'when there is one item' do
+        before { linked_list.insert('item1') }
+        it { should eq 'item1' }
+      end
+      context 'when there is more than one item' do
+        before { 3.times { |n| linked_list.insert("item#{n+1}") } }
+        it { should eq 'item3, item2, item1' }
+      end
+    end
   end
 end
